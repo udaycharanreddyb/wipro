8d89d386680477ffe7ba18c3928f3a19617f2f4a
udaycharanreddyb <udaycharanreddy0308@gmail.com>
2025-12-08 14:46:30 +0530

    Add files via upload

---
+++ SpringBootMVCThymeleaf/src/main/java/com/example/SpringBootMvcThymeleafApplication.java
@@ -0,0 +1,14 @@
+
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ SpringBootMVCThymeleaf/src/main/java/com/example/model/User.java
@@ -0,0 +1,32 @@
+
+package com.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="usertable")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String password;
+	
+	public User(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	
+}
