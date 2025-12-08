25d88ad613dad98cdd1069c206a8dc8c17ab28c6
udaycharanreddyb <udaycharanreddy0308@gmail.com>
2025-12-08 14:49:00 +0530

    Add files via upload

---
+++ SpringSecurityFormJdbc/src/main/java/com/example/securitydemo/SpringSecurityFormJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFormJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFormJdbcApplication.class, args);
+	}
+
+}
